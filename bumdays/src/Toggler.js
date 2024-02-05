40c8eaf52462f382b93f9e2b7a94baa3a966c4c9
Jodi Qiao <dqiao100@uottawa.ca>
2024-02-05 16:41:12 -0500

    feat (#15) updated rudimentary ui components and layout

---
+++ bumdays/src/Toggler.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+export default function ToggleButtons() {
+  const [alignment, setAlignment] = React.useState('left');
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  return (
+    <ToggleButtonGroup
+      value={alignment}
+      exclusive
+      onChange={handleAlignment}
+      aria-label="text alignment"
+    >
+      <ToggleButton value="left" aria-label="left aligned">
+        Line
+      </ToggleButton>
+      <ToggleButton value="right" aria-label="right aligned">
+        Bar
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
